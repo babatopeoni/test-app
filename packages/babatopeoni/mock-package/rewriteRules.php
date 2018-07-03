57c5b6b26f673118295fd701e2152c3c68802e86
Babatope Oni <babatopeoni@gmail.com>
2018-07-03 14:00:32 -0700

    packages added

--- packages/babatopeoni/mock-package/rewriteRules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: babatopeoni, mock-package, babatopeoni, mock-package
-return [
-    'src/MyPackage.php' => 'src/mock-package.php',
-    'config/mypackage.php' => 'config/mock-package.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/mock-package.php',
-    'src/MyPackageServiceProvider.php' => 'src/mock-packageServiceProvider.php',
-];
--- packages/babatopeoni/mock-package/src/mock-package.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace babatopeoni\mock-package;
-
-class mock-package
-{
-    // Build wonderful things
-}
